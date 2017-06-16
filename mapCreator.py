568fddfb9e0cae6b04a83147daa825ab4a7d35b8
Sassaro Titouan <sassarotit@eisti.eu>
2017-06-16 13:12:57 +0200

    ajout_createurMap

---
+++ mapCreator.py
@@ -0,0 +1,77 @@
+from tkinter import *
+from os import system
+from math import sqrt
+
+def distance(x1, y1, x2, y2):
+    return sqrt((x1-x2)**2+(y1-y2)**2)
+
+def switchColor(event):
+    global blockchecker, mode
+    found = False
+    plyrspotted = False
+    for i in blockchecker:
+        for j in i:
+            if j.color == '#27ADF9' and mode == '#27ADF9':
+                plyrspotted = True
+            if j.color == '#27ADF9' and plyrspotted:
+                j.switch('white')
+            elif distance(event.x, 0, j.x, 0) < 10 and distance(event.y, 0, j.y, 0) < 10:
+                j.switch(mode)
+
+def switchMode(c):
+    global mode
+    mode = c
+
+def createmap():
+    global blockchecker
+    f = open('terrain.data', 'w')
+    for i in blockchecker:
+        for j in i:
+            if j.color == 'black':
+                f.write('#')
+            elif j.color == 'white':
+                f.write(' ')
+            elif j.color == 'yellow':
+                f.write('C')
+            else:
+                f.write('P')
+        f.write('\n')
+    f.close()
+    root.destroy()
+    system('python3 minigame.py')
+
+class blockcheck:
+    def __init__(self, x, y, color = 'white'):
+        self.color = color
+        self.x, self.y = x+10, y+10
+        self.body = c.create_rectangle(x, y, x+20, y+20, fill = color, outline = '#168BCE')
+
+    def switch(self, color):
+        self.color = color
+        c.itemconfig(self.body, fill = color)
+
+root = Tk()
+root.title('Createur de map')
+root.configure(bg = '#168BCE')
+
+mode = 'black'
+
+c = Canvas(root, width = 600, height = 400)
+c.bind('<Button-1>', switchColor)
+c.bind('<B1-Motion>', switchColor)
+
+blockchecker = []
+for i in range(20):
+    temp = []
+    for j in range(30):
+        temp.append(blockcheck(j*20, i*20))
+    blockchecker.append(temp)
+
+c.pack(side = LEFT, padx = 20, pady = 20)
+Label(root, text = 'Créez votre niveau et jouez-y:\nIl faut un seul personnage, des murs et au moins une pièce.', bg = '#168BCE', fg = 'white').pack(padx = 20, pady = 20)
+Button(root, text = 'ajouter des murs', command = lambda: switchMode('black'), fg = 'white', bg = '#168BCE', activebackground = '#27ADF9', activeforeground = 'white', relief = FLAT).pack(padx = 20, pady = 20)
+Button(root, text = 'supprimer les elements', command = lambda: switchMode('white'), fg = 'white', bg = '#168BCE', activebackground = '#27ADF9', activeforeground = 'white', relief = FLAT).pack(padx = 20, pady = 20)
+Button(root, text = 'ajouter des pieces', command = lambda: switchMode('yellow'), fg = 'white', bg = '#168BCE', activebackground = '#27ADF9', activeforeground = 'white', relief = FLAT).pack(padx = 20, pady = 20)
+Button(root, text = 'ajouter le personnage', command = lambda: switchMode('#27ADF9'), fg = 'white', bg = '#168BCE', activebackground = '#00BBFF', activeforeground = 'white', relief = FLAT).pack(padx = 20, pady = 20)
+Button(root, text='Creer la carte', command = createmap, fg = 'white', bg = '#168BCE', activebackground = '#27ADF9', activeforeground = 'white', relief = FLAT).pack(padx = 20, pady = 20)
+root.mainloop()
--- minigame.py
+++ minigame.py
@@ -39,67 +39,65 @@ def releasecontrols(event):
 
 # -- creation des classes --#
 class perso(object):
-    def __init__(self):
+    def __init__(self, x, y):
         global size
         self.a = [0,1]
         self.v = [0,0]
         self.speed = 3
-        self.x = w/2
-        self.y = h/2
-        self.pv = 3
-        self.color = 'red'
-        self.body = c.create_rectangle(self.x-size, self.y-size, self.x+size, self.y+size, fill = self.color)
+        self.x = x
+        self.y = y
+        self.w = 3*size/4
+        self.body = c.create_rectangle(self.x-self.w, self.y-self.w, self.x+self.w, self.y+self.w, fill = '#27ADF9')
         self.update()
 
     def show(self):
         global w, h
-        if self.pv == 3:
-            self.color = 'red'
-        elif self.pv == 2:
-            self.color = 'orange'
-        elif self.pv == 1:
-            self.color = 'black'
-
-        if self.pv > 0:
-            c.coords(self.body, self.x-size, self.y-size, self.x+size, self.y+size)
-            c.itemconfig(self.body, fill = self.color)
-        else:
-            c.delete(self.body)
+        c.coords(self.body, self.x-self.w, self.y-self.w, self.x+self.w, self.y+self.w)
 
     def ishitting(self):
         global size, jump
+        for i in coins:
+            if distance(self.x, self.y, i.x, i.y) < size+self.w:
+                c.delete(i.body)
+                coins.remove(i)
+            if len(coins) == 0:
+                root.quit()
         for i in terrain:
-            c.itemconfig(i.body, fill = 'white')
-            if self.v[1] < 0 and distance(self.x, 0, (i.x+i.X)/2, 0) < 2*size:
-                if distance(self.y, 0, i.Y, 0) <= size:
+            if self.v[1] < 0 and distance(self.x, 0, (i.x+i.X)/2, 0) < size+self.w:
+                if distance(self.y, 0, i.Y, 0) < size:
                     self.v[1] = 0
-                    self.y = i.Y+size
-            elif self.v[1] > 0 and distance(self.x, 0, (i.x+i.X)/2, 0) < 2*size:
+                    self.y = i.Y+self.w
+            elif self.v[1] > 0 and distance(self.x, 0, (i.x+i.X)/2, 0) < size+self.w:
                 if distance(self.y, 0, i.y, 0) < size:
                     self.v[1] = 0
-                    self.y = i.y-size
+                    self.y = i.y-self.w
                     jump = 0
                 elif distance(self.y, 0, i.y, 0) == size:
                     self.v[1] = 0
                     jump = 0
 
-            if self.v[0] < 0 and distance(self.y, 0, (i.y+i.Y)/2, 0) < 2*size:
+            if self.v[0] < 0 and distance(self.y, 0, (i.y+i.Y)/2, 0) < size+self.w:
                 if distance(self.x, 0, i.X, 0) < size:
                     self.v[0] = 0
-                    self.x = i.X + size
-                    jump = 0
+                    if self.v[1] >= 0:
+                        self.v[1] = 0.2
+                    self.x = i.X + self.w
+                    jump = 1
                 elif distance(self.x, 0, i.X, 0) == size:
                     self.v[0] = 0
-                    jump = 0
-            elif self.v[0] > 0 and distance(self.y, 0, (i.y+i.Y)/2, 0) < 2*size:
+                    jump = 1
+            elif self.v[0] > 0 and distance(self.y, 0, (i.y+i.Y)/2, 0) < size+self.w:
                 if distance(self.x, 0, i.x, 0) < size:
                     self.v[0] = 0
-                    self.x = i.x - size
-                    jump = 0
-                elif distance(self.x, 0, i.x, 0) == size:
+                    if self.v[1] >= 0:
+                        self.v[1] = 0.2
+                    self.x = i.x - self.w
+                    jump = 1
+                elif distance(self.x, 0, i.x, 0) == self.w:
                     self.v[0] = 0
-                    jump = 0
-
+                    jump = 1
+            if self.x + self.w < 0 or self.x - self.w > w or self.y + self.w < 0 or self.y - self.w > h:
+                root.quit()
 
     def update(self):
         self.v[0] += self.a[0]
@@ -110,22 +108,23 @@ class perso(object):
         self.show()
         root.after(20, self.update)
 
-class block(object):
+class block:
     def __init__(self, x, y):
         self.x, self.y = x, y
         self.X, self.Y = x+2*size, y+2*size
-        self.body = c.create_rectangle(x, y, self.X, self.Y, fill = 'white')
+        self.body = c.create_rectangle(x, y, self.X, self.Y, fill = '#202020', outline = '#202020')
+
+class coin:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+        self.body = c.create_oval(x-size, y-size, x+size, y+size, fill ='yellow')
 
 
 #-- programme principal --#
 mapArray = lire_txt('terrain.data')
 size = 10
 h = len(mapArray)*2*size
-if h > 0:
-    w = len(mapArray[0])*2*size
-else:
-    w = 640
-print(w,h)
+w = len(mapArray[0])*2*size
 holdkey = 'none'
 ismoving = None
 keyhist = []
@@ -135,20 +134,31 @@ system('xset r off')
 
 root = Tk()
 root.title('mini_game')
-c = Canvas(root, width = w, height = h)
+c = Canvas(root, width = w, height = h, bg = 'white')
 c.focus_set()
 c.bind('<KeyPress>', controls)
 c.bind('<KeyRelease>', releasecontrols)
 
 terrain = []
+coins = []
 for i in range(len(mapArray)):
     for j in range(len(mapArray[0])):
         if mapArray[i][j] == '#':
             terrain.append(block(2*j*size, 2*i*size))
-
-plyr = perso()
+        elif mapArray[i][j] == 'P':
+            plyr = perso(2*j*size+size, 2*i*size+size)
+        elif mapArray[i][j] == 'C':
+            coins.append(coin(2*j*size+size, 2*i*size+size))
+if len(coins) == 0:
+    nocoins = True
+else:
+    nocoins = False
 moving()
 
 c.pack()
 root.mainloop()
 system('xset r on')
+if len(coins) == 0 and not(nocoins):
+    print('Vous avez gagné')
+else:
+    print('Vous avez perdu')
